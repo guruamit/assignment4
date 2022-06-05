d6c9ac54720b5da4d47feb4d7269e39ced8a0de9
guruamit <105117009+guruamit@users.noreply.github.com>
2022-06-05 17:59:59 +0530

    Add files via upload

---
+++ assignment4_q_2.py
@@ -0,0 +1,4 @@
+#Write a Python program to triple all numbers of a given list of integers. Use Python map
+sample_list = [ 1, 2, 3, 4, 5, 6, 7]
+result =list( map(lambda a: a*3,sample_list))
+print(result)
